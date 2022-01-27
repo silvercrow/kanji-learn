70387b906ceb79ae7f9019655bea563ed872f8b7
David Balan <david@conchalabs.com>
2022-01-27 00:44:01 -0500

    Second Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 addEventListener('fetch', (event) => { 
-    const response = new Response('Hello David :D', {
+    const response = new Response('Hello David :D this is the second update', {
         headers: { 'Content-Type': 'text/plain' },
     });
     event.respondWith(response);
