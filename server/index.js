ded60c295c78878c3b0d83f990ee2f1054654a31
David Balan <david@conchalabs.com>
2022-01-27 17:21:02 -0500

    refactor

--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const messages = [];
+const channel = new BroadcastChannel("chat");
+channel.onmessage = (event) => {
+  messages.push(event.data);
+};
+
 const router = new Router();
 router
   .get("/", (context) => {
-    context.response.body = "Hello David Im a Router!";
+    context.response.body = "Chat server!";
   })
   .get("/messages", (context) => {
-    context.response.body = messages
+    context.response.body = messages;
   })
-  .post("/messages", async(context) => {
+  .post("/messages", async (context) => {
     const message = await context.request.body().value;
     messages.push(message);
+    channel.postMessage(message);
     context.response.body = messages;
   });
 
 const app = new Application();
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
