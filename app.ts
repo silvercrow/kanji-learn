04510a3482d197099b895de62ab931b9b2767afd
David Balan <david@conchalabs.com>
2022-01-27 23:35:07 -0500

    New Refactor

--- app.ts
+++ app.ts
@@ -1,4 +1,10 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import {
+    graphql,
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+  }  from "https://raw.githubusercontent.com/adelsz/graphql-deno/v15.0.0/mod.ts";
 
 const router = new Router();
 router.get("/", (ctx) => {
@@ -13,4 +19,30 @@ app.addEventListener(
   "listen",
   (e) => console.log("Listening on http://localhost:8080"),
 );
+
+
+const query = '{ hello }';
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        },
+      },
+    },
+  }),
+});
+
+graphql(schema, query).then((result: any) => {
+  // Prints
+  // {
+  //   data: { hello: "world" }
+  // }
+  console.log(result);
+});
+
 await app.listen({ port: 8080 });
